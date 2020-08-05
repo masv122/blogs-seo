0fd3a600ace9c865ac51ec6c064fa842691e4f34
Marcel <masv122@gmail.com>
2020-08-05 16:10:59 -0400

    (06-08-20): conexion creada, carga de post añadida

--- Pradiant/index.php
+++ Pradiant/index.php
@@ -47,52 +47,53 @@
     <main role="main" class="container mb-3">
         <div class="row">
             <div class="col-md-8 blog-main">
-                <?php 
+                <?php
+                try {
                 $conexion = mysqli_connect("localhost", "root", "", "pradiant_blog");
-                if(!$conexion){
-                    echo "la conexion ha fallado: " . mysqli_error();
-                    exit();
-                }
-                $consulta = "SELECT * FROM post ORDER BY fecha DESC";
-                if($resultado = mysqli_query($conexion, $consulta)){
-                    while($registro = mysqli_fetch_assoc($resultado)){
-                        echo"<div class="card bg-white shadow mb-3">"
-                            <div class="card-body">
-                                <h2 class="blog-post-title">Blog post</h2>
-                                <div class="d-flex">
-                                    <p class="blog-post-meta mr-3"><i class="fas fa-clock    "></i> 3 de agosto del 2010</p>
-                                    <p class="blog-post-meta mr-3"><i class="fa fa-user" aria-hidden="true"></i> Fulanito</p>
-                                    <p class="blog-post-meta mr-3"><i class="fa fa-folder" aria-hidden="true"></i> Categoria</p>
-                                    <p class="blog-post-meta mr-3"><i class="fa fa-comment" aria-hidden="true"></i> Comentarios
-                                    </p>
-                                </div>
-        
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-6 border">
-                                            <h6>Imagen</h6>
-                                        </div>
-                                        <div class="col-6">
-                                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                                Explicabo itaque
-                                                architecto qui nulla dolores esse modi omnis eum soluta deserunt repudiandae,
-                                                placeat
-                                                eligendi magnam, officiis error excepturi commodi dolore quisquam.</p>
+                    if(!$conexion){
+                        echo "la conexion ha fallado: " . mysqli_error();
+                        exit();
+                    }
+                    $consulta = "SELECT * FROM posts ORDER BY fecha DESC";
+                    $resultado = mysqli_query($conexion, $consulta);
+                    if($resultado){
+                        while($registro = mysqli_fetch_assoc($resultado)){
+                            echo "<div class='card bg-white shadow mb-3'>
+                                 <div class='card-body'>
+                                    <h2 class='blog-post-title'>" . $registro["titulo"] . "</h2>
+                                    <div class='d-flex'>
+                                        <p class='blog-post-meta mr-3'><i class='fas fa-clock    '></i>" . $registro["resumen"] . "</p>
+                                        <p class='blog-post-meta mr-3'><i class='fa fa-user' aria-hidden='true'></i> Fulanito</p>
+                                        <p class='blog-post-meta mr-3'><i class='fa fa-folder' aria-hidden='true'></i>" . $registro["categoria"] . "</p>
+                                        <p class='blog-post-meta mr-3'><i class='fa fa-comment' aria-hidden='true'></i> Comentarios
+                                        </p>
+                                    </div>
+            
+                                    <div class='container'>
+                                        <div class='row'>
+                                            <div class='col-6 border'>
+                                                <h6>Imagen</h6>
+                                            </div>
+                                            <div class='col-6'>
+                                                <p class='card-text'>" . $registro["resumen"] . "</p>
+                                            </div>
                                         </div>
                                     </div>
+            
                                 </div>
-        
-                            </div>
-                            <div class="card-footer text-muted">
-                                <a name="asdasd" id="asdasd" class="btn btn-dark float-right" href="posts/post.html"
-                                    role="button">
-                                    Ir
-                                    al
-                                    post <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
+                                <div class='card-footer text-muted'>
+                                    <a name='asdasd' id='asdasd' class='btn btn-dark float-right' href='" . $registro["direccion"] . "'
+                                        role='button'>
+                                        Ir
+                                        al
+                                        post <i class='fa fa-arrow-circle-right' aria-hidden='true'></i></a>
+                                </div>
+                            </div>";
+                        }
                     }
-                }
+                } catch (\Throwable $th) {
+                    echo"error";
+                } 
                 ?>
                 <nav aria-label="Page navigation">
                     <ul class="pagination justify-content-center">
